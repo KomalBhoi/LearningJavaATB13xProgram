09a2482690fe1f102b7da69b4de27c7c62cf9396
Komal Bhoi <bhoikomalk@gmail.com>
2025-06-12 15:43:22 +0530

    Print table with printf statement.

--- src/ex_01_Java_Basic/Lab011_Printf.java
+++ src/ex_01_Java_Basic/Lab011_Printf.java
@@ -4,15 +4,15 @@ public class Lab011_Printf {
     public static void main(String[] args) {
         int number = 9;
 
-        System.out.printf("%s\t%s\t\n", number + "x" + 1 + " = ",number*1);
-        System.out.printf("%s\t%s\t\n", number + "x" + 2 + " = ",number*2);
-        System.out.printf("%s\t%s\t\n", number + "x" + 3 + " = ",number*3);
-        System.out.printf("%s\t%s\t\n", number + "x" + 4 + " = ",number*4);
-        System.out.printf("%s\t%s\t\n", number + "x" + 5 + " = ",number*5);
-        System.out.printf("%s\t%s\t\n", number + "x" + 6 + " = ",number*6);
-        System.out.printf("%s\t%s\t\n", number + "x" + 7 + " = ",number*7);
-        System.out.printf("%s\t%s\t\n", number + "x" + 8 + " = ",number*8);
-        System.out.printf("%s\t%s\t\n", number + "x" + 9 + " = ",number*9);
-        System.out.printf("%s\t%s\t\n", number + "x" + 10 + " = ",number*10);
+        System.out.printf("%s\t%s\t\n", number + " x " + 1 + " = ",number*1);
+        System.out.printf("%s\t%s\t\n", number + " x " + 2 + " = ",number*2);
+        System.out.printf("%s\t%s\t\n", number + " x " + 3 + " = ",number*3);
+        System.out.printf("%s\t%s\t\n", number + " x " + 4 + " = ",number*4);
+        System.out.printf("%s\t%s\t\n", number + " x " + 5 + " = ",number*5);
+        System.out.printf("%s\t%s\t\n", number + " x " + 6 + " = ",number*6);
+        System.out.printf("%s\t%s\t\n", number + " x " + 7 + " = ",number*7);
+        System.out.printf("%s\t%s\t\n", number + " x " + 8 + " = ",number*8);
+        System.out.printf("%s\t%s\t\n", number + " x " + 9 + " = ",number*9);
+        System.out.printf("%s\t%s\t\n", number + " x " + 10 + " = ",number*10);
     }
 }
